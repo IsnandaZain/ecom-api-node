933c2f75aadfac398563c789c2240a135dd75a7d
isnandazain <bitartikel@gmail.com>
2020-05-23 00:52:20 +0700

    add endpoint testing

--- server/routes/auth/user.route.js
+++ server/routes/auth/user.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import * as userCtrl from '../../controllers/user.controller';
 
 const router = express.Router();
 
 router.route('/:id')
     /** GET /api/v1/user/:id - Get user by id */
     .get( (req, res, next) => {
-        userCtrl.get(req, res, next)
+        return res.json({
+            "status": 200,
+            "messages": "Sebagai bukti bahwa sukses running" 
+        })
     });
 
 export default router;
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './auth/auth.route';
 import searchRoutes from './search.route';
+import userRoutes from './auth/user.route';
 import adminProduct from './admin_room/product.route';
 import adminCategory from './admin_room/category.route';
 
@@ -9,6 +10,8 @@ const router = express.Router();
 router.use('/auth', authRoutes);
 
 router.use('/search', searchRoutes);
+
+router.use('/user', userRoutes);
 
 router.use('/dashboard/product', adminProduct);
 
