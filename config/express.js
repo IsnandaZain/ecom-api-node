40537db9bffc989260a14ce91cee7a1a24134ae7
isnandazain <bitartikel@gmail.com>
2020-05-23 01:17:16 +0700

    update config express

--- config/express.js
+++ config/express.js
@@ -12,8 +12,8 @@ const app = express();
 
 require('dotenv').config();
 
-app.set('port', process.env.APP_PORT || 3000);
-app.set('host', process.env.APP_HOST || 'localhost');
+app.set('port', process.env.PORT || 3000);
+app.set('host', process.env.HOST || '0.0.0.0');
 
 // hooks for before and after request
 app.use( async function(req, res, next) {
